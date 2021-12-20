e0903918e2850c7e2681005a327f0a8d89e3c5b7
Kyle Mcburnett <kylemcburnett@xingpeds.com>
2021-12-20 11:04:31 -0800

    new android project

---
+++ androidApp/app/src/main/java/com/xingpeds/animaltraining/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.xingpeds.animaltraining
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.xingpeds.animaltraining.ui.theme.AnimalTrainingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimalTrainingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AnimalTrainingTheme {
+        Greeting("Android")
+    }
+}
